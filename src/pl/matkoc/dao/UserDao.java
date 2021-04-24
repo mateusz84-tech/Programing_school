cdb063459cabc8c73250d78cdc522d78342dff73
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-04-24 09:25:15 +0200

    I created UserDao class with method create.

--- src/pl/matkoc/dao/UserDao.java
+++ src/pl/matkoc/dao/UserDao.java
@@ -1,16 +1,45 @@
 package pl.matkoc.dao;
 
+import pl.matkoc.connection.DBUtil;
+import pl.matkoc.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserDao {
 
-    private static final String CREATE_USER_QUERY =
+    private final String CREATE_USER_QUERY =
             "INSERT INTO users(username,email,password,user_group_id)VALUES(?,?,?,?)";
-    private static final String READ_USER_QUERY =
+    private final String READ_USER_QUERY =
             "SELECT * FORM users WHERE id = ?";
-    private static final String UPDATE_USER_QUERY =
+    private final String UPDATE_USER_QUERY =
             "UPDATE users SET username = ?, email = ?, password = ? user_group_id = ? WHERE id = ?";
-    private static final String DELETE_USER_QUERY =
+    private final String DELETE_USER_QUERY =
             "DELETE FROM users WHERE id = ?";
-    private static final String FIND_ALL_USERS =
+    private final String FIND_ALL_USERS =
             "SELECT * FROM users";
+
+    public User create(User user){
+        try(Connection connection = DBUtil.getConnection()){
+            PreparedStatement statement = connection.prepareStatement(CREATE_USER_QUERY,
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+            statement.setString(1,user.getUserName());
+            statement.setString(2,user.getEmail());
+            statement.setString(3,user.getPassword());
+            statement.setInt(4,user.getGroup().getId());
+            statement.executeUpdate();
+
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if(resultSet.next()){
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        }catch (SQLException exc){
+            exc.printStackTrace();
+            return null;
+        }
+    }
 
 }
