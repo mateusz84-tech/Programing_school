6b127c0ad35d28fd8e4f8a4919889964e77113b6
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-04-24 08:30:51 +0200

    I changed method getConnection in DBUtil class.

--- src/pl/matkoc/connection/DBUtil.java
+++ src/pl/matkoc/connection/DBUtil.java
@@ -6,11 +6,10 @@ import java.sql.SQLException;
 
 public class DBUtil {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/programing_school?serverTimezone=UTC&useSSL=false&characterEncoding=utf8";
-    private static final String USER = "root";
-    private static final String PASSWORD = "password";
-
     public static Connection getConnection() throws SQLException{
-         return DriverManager.getConnection(URL,USER,PASSWORD);
+         return DriverManager.getConnection(
+                 "jdbc:mysql://localhost:3306/programing_school?serverTimezone=UTC&useSSL=false&characterEncoding=utf8",
+                 "root",
+                 "coderslab");
     }
 }
