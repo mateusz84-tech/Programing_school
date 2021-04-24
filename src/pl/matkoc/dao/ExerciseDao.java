304d49e0c759f2863bf61ae1d1581a65ef6764bd
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-04-24 18:31:36 +0200

    I wrote function that writes new data to the table exercise.

--- src/pl/matkoc/dao/ExerciseDao.java
+++ src/pl/matkoc/dao/ExerciseDao.java
@@ -1,5 +1,13 @@
 package pl.matkoc.dao;
 
+import pl.matkoc.connection.DBUtil;
+import pl.matkoc.model.Exercise;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ExerciseDao {
 
     private final String CREATE_EXERCISE_QUERY =
@@ -12,4 +20,22 @@ public class ExerciseDao {
             "DELETE FROM exercise WHERE id = ?";
     private final String GET_ALL_EXERCISE =
             "SELECT * FORM exercise";
+
+    public Exercise create(Exercise exercise){
+        try(Connection connection = DBUtil.getConnection()) {
+            PreparedStatement statement =
+                    connection.prepareStatement(CREATE_EXERCISE_QUERY,PreparedStatement.RETURN_GENERATED_KEYS);
+            statement.setString(1, exercise.getTitle());
+            statement.setString(2,exercise.getDescription());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            while(resultSet.next()){
+                exercise.setId(resultSet.getInt(1));
+            }
+            return exercise;
+        }catch (SQLException exc){
+            exc.printStackTrace();
+            return null;
+        }
+    }
 }
