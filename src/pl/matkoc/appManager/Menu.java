2bb9830de13e8587a5513e4f7e523c9712a1f3b4
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-04-27 21:48:21 +0200

    Created class Menu with method display and choice.

---
+++ src/pl/matkoc/appManager/Menu.java
@@ -0,0 +1,45 @@
+package pl.matkoc.appManager;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void display(){
+        System.out.println();
+        System.out.printf("%20s%n\t","====== MENU =======");
+        System.out.printf("\t%-20s%n\t","1. Dodaj");
+        System.out.printf("\t%-20s%n\t","2. Edytuj");
+        System.out.printf("\t%-20s%n\t","3. Usuń");
+        System.out.printf("\t%-20s%n\t","4. Zakończ");
+    }
+
+    public static void getChoiceMenu(){
+
+        Scanner input = new Scanner(System.in);
+        int choice = 0;
+        System.out.print("Wybierz: ");
+        try {
+            choice = input.nextInt();
+        }catch (InputMismatchException exc){
+            System.out.println("Błędne dane wejściowe.");
+        }
+        switch(choice){
+            case 1:{
+                System.out.println("Dodawanie...");
+            }break;
+            case 2:{
+                System.out.println("Edycja...");
+            }break;
+            case 3:{
+                System.out.println("Usuwanie...");
+            }break;
+            case 4:{
+                System.out.println("Dzięki za skorzystanie z programu.");
+                System.exit(0);
+            }
+            default:
+                System.out.println("Brak takiej opcji w manu.");
+        }
+    }
+}
