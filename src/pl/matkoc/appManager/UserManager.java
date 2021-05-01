2ba823cb1d6e8af37230c6470d1ebf17ef46b7fd
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-05-01 21:08:08 +0200

    Created class user manager to manage option user.

--- src/pl/matkoc/appManager/UserManager.java
+++ src/pl/matkoc/appManager/UserManager.java
@@ -1,43 +1,32 @@
 package pl.matkoc.appManager;
 
 import pl.matkoc.dao.ExerciseDao;
-import pl.matkoc.dao.GroupDao;
-import pl.matkoc.dao.SolutionDao;
 import pl.matkoc.dao.UserDao;
 import pl.matkoc.model.Exercise;
-import pl.matkoc.model.Group;
-import pl.matkoc.model.Solution;
 import pl.matkoc.model.User;
+import pl.matkoc.service.CrudFacade;
 import pl.matkoc.service.CrudManager;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class UserManager {
 
     public static void main(String[] args) {
 
-        ExerciseDao exerciseDao = new ExerciseDao();
-        CrudManager<Exercise,Integer> crudManager = exerciseDao;
-        System.out.println(crudManager.read(1));
-        System.out.println(crudManager.findAll());
-        CrudManager<Group,Integer> crudManager1 = new GroupDao();
-        System.out.println(crudManager1.read(1));
-        System.out.println(crudManager1.findAll());
-        SolutionDao solutionDao = new SolutionDao();
-        CrudManager<Solution,Integer> crudManager2 = solutionDao;
-        System.out.println(crudManager2.findAllByUserId(1, solutionDao.getFIND_ALL_BY_USER_ID_QUERY()));
-        System.out.println(crudManager2.findAllByExerciseId(1, solutionDao.getFIND_ALL_BY_EXERCISE_ID_QUERY()));
-
-        UserDao userDao = new UserDao();
-        CrudManager<User,Integer> crudManager3 = userDao;
-        System.out.println("Metoda z USERMANAGER: " + crudManager3.findAllByExerciseId(1,userDao.getFIND_ALL_USERS_BY_GROUP_ID()));
-
+        CrudFacade facade = new CrudFacade();
+        System.out.println("Zarządzanie użytkownikami.");
+        for(;;) {
+            displayAllUser(facade);
+            Menu.displayActionOptions();
+            selectUserAction(facade);
+        }
 
     }
 
-    public static void displayAllUser(UserDao userDao){
+    public static void displayAllUser(CrudFacade allUser){
 
-        List<User> userList = userDao.findAll();
+        List<User> userList = allUser.findAllUser();
         System.out.println("===================== LISTA UŻYTKOWNIKÓW =======================");
         System.out.printf("%-6s\t%-18s\t%-24s\t%-18s%n","ID","NAME","EMAIL","GROUP_ID");
         for(int i=1; i<=64; i++){
@@ -50,4 +39,71 @@ public class UserManager {
                     user.getGroupId());
         }
     }
+
+    public static void selectUserAction(CrudFacade crudFacade){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Wybierz: ");
+        int choice = input.nextInt();
+        switch (choice){
+            case 1: {
+                Scanner scanner = new Scanner(System.in);
+                User user = new User();
+                System.out.println("Dodawanie nowego użtkonika.");
+                System.out.print("Podaj nazwę użytkownika: ");
+                user.setUserName(scanner.nextLine());
+                System.out.print("Podaj email: ");
+                user.setEmail(scanner.nextLine());
+                System.out.print("Podaj hasło: ");
+                user.setPassword(scanner.nextLine());
+                System.out.print("Podaj id grupy do któtej ma należeć: ");
+                user.setGroupId(scanner.nextInt());
+                crudFacade.createUser(user);
+                System.out.println("Dodano nowego użytkownika.");
+            }break;
+            case 2: {
+                System.out.println("Pobierz użytkownika z bazy.");
+                System.out.print("Podaj id użytkownika: ");
+                int idUser = input.nextInt();
+                System.out.println(crudFacade.readUser(idUser));
+            }break;
+            case 3:{
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Edycja użytkownika.");
+                System.out.print("Podaj id użytkownika: ");
+                int idUser = scanner.nextInt();
+                User userEdit = crudFacade.readUser(idUser);
+                System.out.print("Wpisz nową nazwę: ");
+                String name = scanner.next();
+                userEdit.setUserName(name);
+                System.out.print("Wpisz nowy email: ");
+                String email = scanner.next();
+                userEdit.setEmail(email);
+                System.out.print("Wpisz nowe hasło: ");
+                String password = scanner.next();
+                userEdit.setPassword(password);
+                System.out.print("Przypisz nową grupę: ");
+                int group = scanner.nextInt();
+                userEdit.setGroupId(group);
+                System.out.print("Potwierdź id użytkownika: ");
+                int id = scanner.nextInt();
+                userEdit.setId(id);
+                crudFacade.updateUser(userEdit);
+                System.out.println("Dane zostały zaktualizowane.");
+            }break;
+            case 4:{
+                System.out.println("Usuwanie użytkownika.");
+                System.out.print("Wpisz id użytkownika do usunięcia: ");
+                int deleteIdUser = input.nextInt();
+                crudFacade.deleteUser(deleteIdUser);
+                System.out.println("Użytkownik został usunięty z bazy.");
+            }break;
+            case 5: {
+                System.out.println("Dziękuje za skorzystanie z programu.");
+                System.exit(0);
+            }break;
+            default:
+                System.out.println("Nie ma takiej opcji w menu.");
+        }
+    }
+
 }
