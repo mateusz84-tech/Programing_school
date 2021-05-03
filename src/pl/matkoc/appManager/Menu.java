0c5edd86d24e8ed4ac47cac4e29cc4d9fb8cd80c
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-05-03 10:25:39 +0200

    Created new function to display solution action.

--- src/pl/matkoc/appManager/Menu.java
+++ src/pl/matkoc/appManager/Menu.java
@@ -1,5 +1,7 @@
 package pl.matkoc.appManager;
 
+import java.io.PrintStream;
+
 public class Menu {
 
     public static void displaySubject(){
@@ -21,5 +23,11 @@ public class Menu {
         System.out.printf("\t%-20s%n\t","4. Usuń");
         System.out.printf("\t%-20s%n\t","5. Wyjdź.");
     }
-
+    public static void displaySolutionAction(){
+        System.out.println();
+        System.out.printf("%20s%n\t","====== MENU =======");
+        System.out.printf("\t%-20s%n\t","1. Dodaj - przypisanie zadań do użytkownika.");
+        System.out.printf("\t%-20s%n\t", "2. Widok - przeglądanie rozwiązań danego użytkownika.");
+        System.out.printf("\t%-20s%n\t","3. Wyjdź.");
+    }
 }
